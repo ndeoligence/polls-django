8ed8ffefa84a6824a70b5b7ac097516add9398a7
Monde <monde.ngalonkulu@nimbletech.co.za>
2018-01-11 19:33:20 +0200

    tmeplates {for index, detail}

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Choice, Question
 
 
 def index(request):
-    return HttpResponse('<h2>Polls home page</h2><br/>')
+    return render(request, 'polls/index.html', {'questions': Question.objects.order_by('-pub_date')[:5]})
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    choices = question.choice_set.order_by('votes', 'id')
+    ctx = {'question': question, 'choices': choices}
+    return render(request, 'polls/detail.html', ctx)
 
 
 def results(request, question_id):
